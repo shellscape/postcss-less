0413491144e9d892715d478d7bc8f11687fc4e3b
webschik <webschik@gmail.com>
2016-04-30 21:02:28 +0200

    #35 parse escaping in nested rules

--- test/parser/escaping.spec.js
+++ test/parser/escaping.spec.js
@@ -23,5 +23,31 @@ describe('Parser', () => {
             expect(root.last.first.first.prop).to.eql('height');
             expect(root.last.first.first.value).to.eql('calc(~"100vh - @{testVar}")');
         });
+
+        it('parses escaping inside nested rules', () => {
+            const code = `
+                .test1 {
+                    .another-test {
+                        prop1: function(~"@{variable}");
+                    }
+                }
+                
+                .test2 {
+                    prop2: function(~\`@{test}\`);
+                }
+                
+                .test3 {
+                    filter: ~"alpha(opacity='@{opacity}')";
+                }
+            `;
+            const root = parse(code);
+
+            expect(root.nodes[0].first.first.prop).to.eql('prop1');
+            expect(root.nodes[0].first.first.value).to.eql('function(~"@{variable}")');
+            expect(root.nodes[1].first.prop).to.eql('prop2');
+            expect(root.nodes[1].first.value).to.eql('function(~`@{test}`)');
+            expect(root.nodes[2].first.prop).to.eql('filter');
+            expect(root.nodes[2].first.value).to.eql('~"alpha(opacity=\'@{opacity}\')"');
+        });
     });
 });
