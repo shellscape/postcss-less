44f0b234ed0645133004e2dbef6779903b910a7c
shellscape <andrew@shellscape.org>
2017-05-12 12:38:38 -0400

    shellscape eslint config, refactoring

--- test/parser/params.spec.js
+++ test/parser/params.spec.js
@@ -1,19 +1,19 @@
 // chai uses expressions for validation
 /* eslint no-unused-expressions: 0 */
 
-import {expect} from 'chai';
+import { expect } from 'chai';
 import parse from '../../lib/less-parse';
 
 describe('Parser', () => {
-    describe('Params', () => {
+  describe('Params', () => {
 
-        it('should not assign parameters for pseudo-selectors (#56)', () => {
-            const code = '.test2:not(.test3) {}';
-            const root = parse(code);
+    it('should not assign parameters for pseudo-selectors (#56)', () => {
+      const code = '.test2:not(.test3) {}';
+      const root = parse(code);
 
-            expect(root.first.selector).to.eql('.test2:not(.test3)');
-            expect(root.first.params).to.be.undefined;
-        });
-
+      expect(root.first.selector).to.eql('.test2:not(.test3)');
+      expect(root.first.params).to.be.undefined;
     });
+
+  });
 });
--- test/postcss.spec.js
+++ test/postcss.spec.js
@@ -3,63 +3,63 @@
 
 import CssSyntaxError from 'postcss/lib/css-syntax-error';
 import Import from '../lib/import';
-import {expect} from 'chai';
+import { expect } from 'chai';
 import lessSyntax from '../lib/less-syntax';
 import postcss from 'postcss';
 
 describe('#postcss', () => {
-    it('should process LESS syntax', (done) => {
-        const lessText = 'a { b {} }';
+  it('should process LESS syntax', (done) => {
+    const lessText = 'a { b {} }';
 
-        postcss()
-            .process(lessText, {syntax: lessSyntax})
-            .then((result) => {
-                expect(result).to.be.not.null;
-                expect(result.css).to.equal(lessText);
-                expect(result.content).to.equal(lessText);
+    postcss()
+      .process(lessText, { syntax: lessSyntax })
+      .then((result) => {
+        expect(result).to.be.not.null;
+        expect(result.css).to.equal(lessText);
+        expect(result.content).to.equal(lessText);
 
-                done();
-            }).catch(done);
-    });
+        done();
+      }).catch(done);
+  });
 
-    it('should parse LESS mixins as at rules', (done) => {
-        const lessText = '.foo (@bar; @baz...) { border: @{baz}; }';
+  it('should parse LESS mixins as at rules', (done) => {
+    const lessText = '.foo (@bar; @baz...) { border: @{baz}; }';
 
-        postcss()
-            .process(lessText, {syntax: lessSyntax})
-            .then((result) => {
-                expect(result).to.be.not.null;
-                expect(result.css).to.equal(lessText);
-                expect(result.content).to.equal(lessText);
+    postcss()
+      .process(lessText, { syntax: lessSyntax })
+      .then((result) => {
+        expect(result).to.be.not.null;
+        expect(result.css).to.equal(lessText);
+        expect(result.content).to.equal(lessText);
 
-                done();
-            }).catch(done);
-    });
+        done();
+      }).catch(done);
+  });
 
-    it('should not parse invalid LESS (#64)', (done) => {
-        const lessText = '.foo';
+  it('should not parse invalid LESS (#64)', (done) => {
+    const lessText = '.foo';
 
-        postcss()
-            .process(lessText, {syntax: lessSyntax})
-            .catch((err) => {
-                expect(err).to.be.an.instanceof(CssSyntaxError);
-                done();
-            });
-    });
+    postcss()
+      .process(lessText, { syntax: lessSyntax })
+      .catch((err) => {
+        expect(err).to.be.an.instanceof(CssSyntaxError);
+        done();
+      });
+  });
 
-    it('should parse @imports as Import', (done) => {
-        const lessText = '@import (inline) "foo.less";';
+  it('should parse @imports as Import', (done) => {
+    const lessText = '@import (inline) "foo.less";';
 
-        postcss()
-            .process(lessText, {syntax: lessSyntax})
-            .then((result) => {
-                expect(result).to.be.not.null;
-                expect(result.css).to.equal(lessText);
-                expect(result.root.first).to.be.an.instanceof(Import);
-                expect(result.root.first.directives).to.equal('(inline)');
-                expect(result.root.first.importPath).to.equal('"foo.less"');
+    postcss()
+      .process(lessText, { syntax: lessSyntax })
+      .then((result) => {
+        expect(result).to.be.not.null;
+        expect(result.css).to.equal(lessText);
+        expect(result.root.first).to.be.an.instanceof(Import);
+        expect(result.root.first.directives).to.equal('(inline)');
+        expect(result.root.first.importPath).to.equal('"foo.less"');
 
-                done();
-            }).catch(done);
-    });
+        done();
+      }).catch(done);
+  });
 });
