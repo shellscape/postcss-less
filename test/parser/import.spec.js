cab6a2db6c59adbfedee01de7200f0831ae0893b
shellscape <andrew@shellscape.org>
2017-05-27 11:11:08 -0400

    fixes #75; import with url() should have importPath

--- test/parser/import.spec.js
+++ test/parser/import.spec.js
@@ -7,51 +7,68 @@ import lessSyntax from '../../lib/less-syntax';
 import postcss from 'postcss';
 
 describe('Parser', () => {
+  describe('Import', () => {
+    it('should parse @imports as Import', (done) => {
+      const lessText = '@import "foo.less";';
 
-  it('should parse @imports as Import', (done) => {
-    const lessText = '@import "foo.less";';
+      postcss()
+              .process(lessText, { syntax: lessSyntax })
+              .then((result) => {
+                expect(result).to.be.not.null;
+                expect(result.css).to.equal(lessText);
+                expect(result.root.first).to.be.an.instanceof(Import);
+                expect(result.root.first.importPath).to.equal('"foo.less"');
 
-    postcss()
-            .process(lessText, { syntax: lessSyntax })
-            .then((result) => {
-              expect(result).to.be.not.null;
-              expect(result.css).to.equal(lessText);
-              expect(result.root.first).to.be.an.instanceof(Import);
-              expect(result.root.first.importPath).to.equal('"foo.less"');
+                done();
+              }).catch(done);
+    });
 
-              done();
-            }).catch(done);
-  });
+    it('should parse @imports with a url function as Import', (done) => {
+      const lessText = '@import url("foo.less");';
 
-  it('should parse @imports as Import, no space', (done) => {
-    const lessText = '@import"foo.less";';
+      postcss()
+              .process(lessText, { syntax: lessSyntax })
+              .then((result) => {
+                expect(result).to.be.not.null;
+                expect(result.css).to.equal(lessText);
+                expect(result.root.first).to.be.an.instanceof(Import);
+                expect(result.root.first.importPath).to.equal('"foo.less"');
+                expect(result.root.first.urlFunc).to.equal(true);
 
-    postcss()
-            .process(lessText, { syntax: lessSyntax })
-            .then((result) => {
-              expect(result).to.be.not.null;
-              expect(result.css).to.equal(lessText);
-              expect(result.root.first).to.be.an.instanceof(Import);
-              expect(result.root.first.importPath).to.equal('"foo.less"');
-              expect(result.root.first.raws.afterName).to.be.undefined;
+                done();
+              }).catch(done);
+    });
 
-              done();
-            }).catch(done);
-  });
+    it('should parse @imports as Import, no space', (done) => {
+      const lessText = '@import"foo.less";';
 
-  it('should parse @imports with directives', (done) => {
-    const lessText = '@import (inline) "foo.less";';
+      postcss()
+              .process(lessText, { syntax: lessSyntax })
+              .then((result) => {
+                expect(result).to.be.not.null;
+                expect(result.css).to.equal(lessText);
+                expect(result.root.first).to.be.an.instanceof(Import);
+                expect(result.root.first.importPath).to.equal('"foo.less"');
+                expect(result.root.first.raws.afterName).to.be.undefined;
 
-    postcss()
-            .process(lessText, { syntax: lessSyntax })
-            .then((result) => {
-              expect(result).to.be.not.null;
-              expect(result.css).to.equal(lessText);
-              expect(result.root.first).to.be.an.instanceof(Import);
-              expect(result.root.first.directives).to.equal('(inline)');
-              expect(result.root.first.importPath).to.equal('"foo.less"');
+                done();
+              }).catch(done);
+    });
 
-              done();
-            }).catch(done);
+    it('should parse @imports with directives', (done) => {
+      const lessText = '@import (inline) "foo.less";';
+
+      postcss()
+              .process(lessText, { syntax: lessSyntax })
+              .then((result) => {
+                expect(result).to.be.not.null;
+                expect(result.css).to.equal(lessText);
+                expect(result.root.first).to.be.an.instanceof(Import);
+                expect(result.root.first.directives).to.equal('(inline)');
+                expect(result.root.first.importPath).to.equal('"foo.less"');
+
+                done();
+              }).catch(done);
+    });
   });
 });
