b60edba65649a3075c4b852349df7ae9a115080f
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2018-09-22 16:58:50 +0200

    fix: crash on inline comments. Closes #114 (#115)
    * Fix crash on inline comments. Closes #114
    
    * chore: use slice for consistency

--- test/parser/comments.test.js
+++ test/parser/comments.test.js
@@ -15,6 +15,18 @@ test('inline comment', (t) => {
   t.is(nodeToString(root), less);
 });
 
+test('inline comment without leading space', (t) => {
+  const less = '//batman';
+  const root = parse(less);
+  const { first } = root;
+
+  t.truthy(root);
+  t.true(first instanceof Comment);
+  t.true(first.inline);
+  t.is(first.text, 'batman');
+  t.is(nodeToString(root), less);
+});
+
 test('close empty', (t) => {
   const less = '// \n//';
   const root = parse(less);
